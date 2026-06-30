ff403aa00027420615007c1c916ccc233a8559aa
Vaibhav Shukla <vaibhavshuklaofficial8586@gmail.com>
2026-07-01 00:54:12 +0530

    Routing Added in main.dart

---
+++ lib/screens/job_list_screen.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class JobListScreen extends StatelessWidget {
+  const JobListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
